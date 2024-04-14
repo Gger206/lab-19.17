067f39098a20205b16ad404f6a95174809c4a4bb
Gger206 <arteml22012006@gmail.com>
2024-04-14 14:46:44 +0300

    testStrlen

---
+++ library.c
@@ -0,0 +1,19 @@
+#include "string/string.h"
+#include <assert.h>
+
+void testStrlen() {
+    const char *str = "Hello, World!";
+    const char *str2 = "";
+    size_t length1 = strlen_(str);
+    size_t length2 = strlen_(str2);
+    assert(length1 == 13 && length2 == 0);
+}
+
+
+
+int main() {
+    testStrlen();
+
+return 0;
+
+}
