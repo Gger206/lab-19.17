17fd4c40065e91e3d76935b6a32f9b5b7fd5931c
Gger206 <arteml22012006@gmail.com>
2024-04-14 15:21:57 +0300

    strlen

---
+++ string/string.h
@@ -0,0 +1,13 @@
+#ifndef INC_STRING__H
+#define INC_STRING__H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
